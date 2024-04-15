32bbdd8531ac7b6b3cd8411642843f682468163c
Simran <itssimran903@gmail.com>
2024-04-15 18:01:20 +0530

    commit

--- todoProject/src/components/AddTodo.jsx
+++ todoProject/src/components/AddTodo.jsx
@@ -1,35 +1,42 @@
+// Importing necessary modules from React and Redux
 import React, { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import {addTodo} from '../store/todoSlice'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../store/todoSlice' // Importing the addTodo action creator
 
+// Functional component for adding a new todo
 const AddTodo = () => {
+    // State for managing the input value
     const [input, setInput] = useState('')
+    // Dispatch function obtained from react-redux
     const dispatch = useDispatch()
 
+    // Function to handle adding a new todo
     const addTodoHandler = (e) => {
-        e.preventDefault()
-        dispatch(addTodo(input))
-        setInput('')
+        e.preventDefault() // Preventing default form submission behavior
+        dispatch(addTodo(input)) // Dispatching the addTodo action with the input text
+        setInput('') // Clearing the input field after adding the todo
     }
 
-
-  return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12 mb-20">
-      <input
-        type="text"
-        className="rounded-full border-[4px] focus:ring-2 border-lime-300 text-base outline-none text-black py-2 px-3 leading-8 transition-colors duration-200 ease-in-out w-3/4 lg:w-2/5"
-        placeholder="Enter a Todo..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="text-white border-[3px] border-lime-300 py-3 px-5 font-extrabold focus:outline-none hover:bg-lime-300 rounded-full text-lg"
-      >
-        +
-      </button>
-    </form>
-  )
+    // JSX for rendering the add todo form
+    return (
+        <form onSubmit={addTodoHandler} className="space-x-3 mt-12 mb-20">
+            {/* Input field for entering todo text */}
+            <input
+                type="text"
+                className="rounded-full border-[4px] focus:ring-2 border-lime-300 text-base outline-none text-black py-2 px-3 leading-8 transition-colors duration-200 ease-in-out w-3/4 lg:w-2/5"
+                placeholder="Enter a Todo..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)} // Handling input change
+            />
+            {/* Button to submit the todo */}
+            <button
+                type="submit"
+                className="text-white border-[3px] border-lime-300 py-3 px-5 font-extrabold focus:outline-none hover:bg-lime-300 rounded-full text-lg"
+            >
+                +
+            </button>
+        </form>
+    )
 }
 
-export default AddTodo
+export default AddTodo // Exporting the AddTodo component
--- todoProject/src/components/Todos.jsx
+++ todoProject/src/components/Todos.jsx
@@ -1,47 +1,62 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeTodo } from "../store/todoSlice";
+import { removeTodo } from "../store/todoSlice"; // Importing removeTodo action creator
 
+// Functional component for displaying todos
 const Todos = () => {
+  // Selecting todos from the Redux store state
   const todos = useSelector((state) => state.todos);
+  // Dispatch function obtained from react-redux
   const dispatch = useDispatch();
+  // State to track completed todos
   const [completed, setCompleted] = useState([]);
 
+  // Function to handle checkbox change
   const handleCheckboxChange = (id) => {
     if (completed.includes(id)) {
+      // If the todo is already marked as completed, remove it from completed array
       setCompleted(completed.filter((item) => item !== id));
     } else {
+      // If the todo is not marked as completed, add it to completed array
       setCompleted([...completed, id]);
     }
   };
 
+  // Rendering todos list
   return (
     <>
       <ul className="mx-auto list-none w-2/3">
+        {/* Mapping through todos array and rendering each todo */}
         {todos.map((todo) => (
           <li
             className={`mt-4 flex rounded-lg justify-between items-center ${
+              // Applying conditional styling based on whether the todo is completed or not
               completed.includes(todo.id) ? "bg-lime-200" : "bg-white"
             } px-4 py-2`}
             key={todo.id}
           >
             <div
               className={`flex items-center ${
+                // Applying conditional styling for completed todos
                 completed.includes(todo.id) ? "line-through text-gray-500" : ""
               }`}
             >
+              {/* Checkbox for marking todo as completed */}
               <input
                 type="checkbox"
                 checked={completed.includes(todo.id)}
-                onChange={() => handleCheckboxChange(todo.id)}
+                onChange={() => handleCheckboxChange(todo.id)} // Handling checkbox change
                 className="mr-2"
               />
+              {/* Todo text */}
               {todo.text}
             </div>
+            {/* Button to remove todo */}
             <button
-              onClick={() => dispatch(removeTodo(todo.id))}
+              onClick={() => dispatch(removeTodo(todo.id))} // Dispatching removeTodo action
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
             >
+              {/* SVG icon for delete */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -53,6 +68,7 @@ const Todos = () => {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
+                  // Path for delete icon
                   d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                 />
               </svg>
@@ -64,4 +80,4 @@ const Todos = () => {
   );
 };
 
-export default Todos;
+export default Todos; // Exporting the Todos component
