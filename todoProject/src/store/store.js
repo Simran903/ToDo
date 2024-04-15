12a167148642b38f589d3a98b4b9730bd8dddd15
Simran <itssimran903@gmail.com>
2024-04-15 17:59:35 +0530

    commit

--- todoProject/src/store/store.js
+++ todoProject/src/store/store.js
@@ -1,6 +1,10 @@
+// Importing the configureStore function from Redux Toolkit
 import { configureStore } from "@reduxjs/toolkit";
+// Importing the todo reducer from the todoSlice file
 import todoreducer from './todoSlice.js'
 
+// Creating the Redux store using configureStore function
 export const store = configureStore({
-    reducer: todoreducer
-})
+    // Configuring the store with the todo reducer
+    reducer: todoreducer // Here, the reducer is set to the reducer function exported from todoSlice.js
+}) 
