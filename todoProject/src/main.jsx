f952b713fa9f3c5d16383050786bee0b54bbf1b2
Simran <itssimran903@gmail.com>
2024-04-15 18:02:56 +0530

    commit

--- todoProject/src/main.jsx
+++ todoProject/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import {store} from './store/store.js'
+import { Provider } from 'react-redux' // Importing Provider from react-redux for providing the Redux store
+import { store } from './store/store.js' // Importing the Redux store
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Wrapping the root component with the Provider to provide access to the Redux store
   <Provider store={store}>
     <App />
   </Provider>,
