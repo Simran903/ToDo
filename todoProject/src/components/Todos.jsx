0ffc2594927feed5bb2df9986b4ec2417e8c8d9a
Simran <itssimran903@gmail.com>
2024-08-19 00:44:36 +0530

    commit

--- todoProject/src/components/Todos.jsx
+++ todoProject/src/components/Todos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../store/todoSlice"; // Importing removeTodo action creator
 
